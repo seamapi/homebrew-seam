82e866c3845b45e5831691dcd8ac5d456be0e351
Andrii <67077601+mastafit@users.noreply.github.com>
2024-02-06 18:02:52 +0100

    Update seam.rb

--- seam.rb
+++ seam.rb
@@ -1,21 +1,20 @@
 require "language/node"
+require "json"
 
 class Seam < Formula
   desc "This utility lets you control Seam resources"
   homepage "https://github.com/seamapi/seam-cli"
-  url "https://github.com/seamapi/seam-cli/archive/refs/tags/0.0.33.tar.gz"
-  sha256 "5a496af613cf30dfa8a0a31bb67be7164321980dde9b4ff1575ae28deac77a56"
+  url "https://registry.npmjs.org/seam-cli/-/seam-cli-0.0.38.tgz"
+  sha256 "fbf397e21c4f6f93db89465f11f819399ef5f76076060a7aa4585d28667129dc"
   license "MIT"
 
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.local_npm_install_args
-    system "npm", "run", "build"
-    bin.install "bin/seam.cjs" => "seam"
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
-  
   test do
-    assert_match version.to_s, shell_output("#{bin}/seam --version")
-  end
+    system "#{bin}/seam", "config", "set", "fake-server"
+  end  
 end
